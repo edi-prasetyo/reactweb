5fd3f09e41790d7f41b9696ab1d16b310f4ca034
edi-prasetyo <ediprasetyo2@gmail.com>
2020-12-10 00:47:39 +0700

    Initial Commit

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import NavbarComponent from './components/NavbarComponent';
+
+function App() {
+  return (
+    <div className="App">
+      <NavbarComponent/>
+    </div>
+  );
+}
+
+export default App;
